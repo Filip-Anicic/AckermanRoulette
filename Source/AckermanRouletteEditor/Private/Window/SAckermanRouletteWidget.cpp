15b96ce3fa64a2809c46da2b31134a89d1af0bc5
Filip Aničić <filip.anicic123@gmail.com>
2023-03-21 19:59:04 +0100

    feat: :sparkles: Added Slate Roulette widget
    Roulette slate widget that holds a details view of the roulette settings and a button to start the roulette.

---
+++ Source/AckermanRouletteEditor/Private/Window/SAckermanRouletteWidget.cpp
@@ -0,0 +1,88 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Window/SAckermanRouletteWidget.h"
+
+#include "SlateOptMacros.h"
+#include "Window/AckermanRouletteToolSettings.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+void SAckermanRouletteWidget::Construct(const FArguments& InArgs)
+{
+	//Custom settings object
+	CustomSettingsObject = GetMutableDefault<UAckermanRouletteToolSettings>();
+	
+	//Load property module
+	FPropertyEditorModule& PropertyModule = FModuleManager::LoadModuleChecked<FPropertyEditorModule>("PropertyEditor");
+
+	//Set Details view settings with FDetailsViewArgs
+	FDetailsViewArgs Args;
+	Args.bHideSelectionTip = false;
+	Args.bAllowSearch = false;
+
+	//Create property widget and set it's object to mutable version of Roulette settings
+	TSharedPtr<IDetailsView> SettingsWidget = PropertyModule.CreateDetailView(Args);
+	SettingsWidget->SetObject(CustomSettingsObject);
+	
+	SpinButton = SNew(SButton)
+		.HAlign(HAlign_Center)
+		.VAlign(VAlign_Center)
+		.Content()
+		[
+			SNew(STextBlock)
+			.Text(FText::FromString(TEXT("Spin roulette")))
+		];
+	
+	ChildSlot
+	[
+	SNew(SOverlay)
+		+ SOverlay::Slot()
+		.HAlign(HAlign_Fill)
+		.VAlign(VAlign_Fill)
+		[
+			//Settings view
+			SNew(SVerticalBox)
+			+ SVerticalBox::Slot()
+			.AutoHeight()
+			.HAlign(HAlign_Fill)
+			.VAlign(VAlign_Fill)
+			[
+				SettingsWidget.ToSharedRef()
+			]
+			
+			+ SVerticalBox::Slot()
+			.AutoHeight()
+			.HAlign(HAlign_Fill)
+			.VAlign(VAlign_Fill)
+			[
+				SNew(SSpacer)
+				.Size(FVector2D(1.0f, 10.0f))
+			]
+			+ SVerticalBox::Slot()
+			.AutoHeight()
+			.HAlign(HAlign_Center)
+			.VAlign(VAlign_Top)
+			[
+				SNew(SHorizontalBox)
+				+ SHorizontalBox::Slot()
+				.HAlign(HAlign_Center)
+				.VAlign(VAlign_Top)
+				[
+					//Spin roulette button
+					SpinButton.ToSharedRef()				
+				]
+			]
+		]
+	];
+
+	
+}
+
+
+UAckermanRouletteToolSettings* SAckermanRouletteWidget::GetCustomSettings() const
+{
+	return CustomSettingsObject;
+}
+
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
---
+++ Source/AckermanRouletteEditor/Public/Window/SAckermanRouletteWidget.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AckermanRouletteToolSettings.h"
+#include "Widgets/SCompoundWidget.h"
+
+/**
+ * 
+ */
+class ACKERMANROULETTEEDITOR_API SAckermanRouletteWidget : public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SAckermanRouletteWidget)
+		{
+		}
+
+	SLATE_END_ARGS()
+
+	/** Constructs this widget with InArgs */
+	void Construct(const FArguments& InArgs);
+	TSharedPtr<SButton> SpinButton;
+	
+private:
+	UAckermanRouletteToolSettings* CustomSettingsObject = nullptr;
+
+public:
+	UAckermanRouletteToolSettings* GetCustomSettings() const;
+};
