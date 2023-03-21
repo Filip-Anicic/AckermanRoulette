bbbef43e0577d5b54825188709e044cf51083ba3
Filip Aničić <filip.anicic123@gmail.com>
2023-03-21 18:34:14 +0100

    refactor: :truck: Fixed typo in AckermanRouletteEditor.Build.cs file name

---
+++ Source/AckermanRouletteEditor/AckermanRouletteEditor.Build.cs
@@ -0,0 +1,31 @@
+﻿using UnrealBuildTool;
+
+public class AckermanRouletteEditor : ModuleRules
+{
+    public AckermanRouletteEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Projects",
+                "InputCore",
+                "EditorFramework",
+                "UnrealEd",
+                "ToolMenus",
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
