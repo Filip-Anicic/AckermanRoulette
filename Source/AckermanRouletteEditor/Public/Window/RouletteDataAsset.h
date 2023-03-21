5b214053e198ea3f29d8f3c0a1d851b3675e9948
Filip Aničić <filip.anicic123@gmail.com>
2023-03-21 17:54:25 +0100

    feat: 🏷️ Add RouletteDataAsset class
    Added a new Primary Data asset class called RouletteDataAsset to act as the data class to store meshes used in the Roulette

---
+++ Source/AckermanRouletteEditor/Public/Window/RouletteDataAsset.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RouletteDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACKERMANROULETTEEDITOR_API URouletteDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, meta=(NoElementDuplicate))
+	TArray<TSoftObjectPtr<UStaticMesh>> RouletteMeshes;
+};
